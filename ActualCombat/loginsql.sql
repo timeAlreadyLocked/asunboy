ad49c7a16d3b026ff9ac09ad5a21ba98f40f9e2b
921295877@qq.com <lipengju9219295877>
2018-07-23 23:07:40 +0800

    feat: 增加module表

--- ActualCombat/loginsql.sql
+++ ActualCombat/loginsql.sql
@@ -1,47 +1,88 @@
-drop database if exists actual_combat;
-create database actual_combat default character set utf8 collate utf8_general_ci;
-use actual_combat;
+DROP DATABASE IF EXISTS actual_combat;
+CREATE DATABASE actual_combat
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+USE actual_combat;
 
-drop table if exists ac_user;
-create table ac_user(
-	id bigint(20) unsigned not null comment '用户ID',
-	nickname varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci not null comment '用户网名',
-	name varchar(64) comment '用户真实姓名',
-	age tinyint unsigned comment '用户真实年龄',
-	sex bit comment '用户性别',
-	mobile varchar(64) comment '用户电话号码',
-	type_id tinyint not null comment '用户类型。管理员自定义类型,不同类型的用户权限不一样',
-	login_pwd varchar(128) not null comment '用户登录密码',
-	pwd_fail_times tinyint  not null default '0' comment '密码登录连续失败次数,登录成功，清除该值',
-	status enum('created','enabled','locked','deleted') not null comment '用户的状态。created(创建的),enabled(启动的),locked(被锁的),deleted(已删除的)',
-	locked_on datetime default null comment '用户被锁时间',
-	last_login_on datetime default null comment '上一次登录成功时间',
-	created_on datetime not null default current_timestamp comment '创建时间',
-	modified_on timestamp not null default current_timestamp on update current_timestamp comment '最后修改时间',
-	primary key (id)
+DROP TABLE IF EXISTS ac_user;
+CREATE TABLE ac_user (
+  id             BIGINT(20) UNSIGNED                              NOT NULL
+  COMMENT '用户ID',
+  nickname       VARCHAR(64)
+                 CHARACTER SET utf8mb4
+                 COLLATE utf8mb4_general_ci                       NOT NULL
+  COMMENT '用户网名',
+  name           VARCHAR(64) COMMENT '用户真实姓名',
+  age            TINYINT UNSIGNED COMMENT '用户真实年龄',
+  sex            BIT COMMENT '用户性别',
+  mobile         VARCHAR(64) COMMENT '用户电话号码',
+  type_id        TINYINT                                          NOT NULL
+  COMMENT '用户类型。管理员自定义类型,不同类型的用户权限不一样',
+  login_pwd      VARCHAR(128)                                     NOT NULL
+  COMMENT '用户登录密码',
+  pwd_fail_times TINYINT                                          NOT NULL DEFAULT '0'
+  COMMENT '密码登录连续失败次数,登录成功，清除该值',
+  status         ENUM ('created', 'enabled', 'locked', 'deleted') NOT NULL
+  COMMENT '用户的状态。created(创建的),enabled(启动的),locked(被锁的),deleted(已删除的)',
+  locked_on      DATETIME                                                  DEFAULT NULL
+  COMMENT '用户被锁时间',
+  last_login_on  DATETIME                                                  DEFAULT NULL
+  COMMENT '上一次登录成功时间',
+  created_on     DATETIME                                         NOT NULL DEFAULT current_timestamp
+  COMMENT '创建时间',
+  modified_on    TIMESTAMP                                        NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+  COMMENT '最后修改时间',
+  PRIMARY KEY (id)
 );
-create table ac_login(
-	login_name varchar(128) comment '用户登录名',
-	login_type_id tinyint not null comment '登录类型。账号密码登录，第三方登录，短信登录，扫码登录',
-	login_target_id bigint(20) not null comment '登录后关联的用户ID或第三方Id',
-	primary key (login_type_id,login_target_id)
+CREATE TABLE ac_login (
+  login_name      VARCHAR(128) COMMENT '用户登录名',
+  login_type_id   TINYINT    NOT NULL
+  COMMENT '登录类型。账号密码登录，第三方登录，短信登录，扫码登录',
+  login_target_id BIGINT(20) NOT NULL
+  COMMENT '登录后关联的用户ID或第三方Id',
+  PRIMARY KEY (login_type_id, login_target_id)
 );
-create table ac_login_type(
-	id tinyint unsigned comment '登录类型id',
-	name varchar(64) not null comment '登录类型名称',
-	enabled bit default 1 not null comment '是否启用',
-	primary key (id)
+CREATE TABLE ac_login_type (
+  id      TINYINT UNSIGNED COMMENT '登录类型id',
+  name    VARCHAR(64)   NOT NULL
+  COMMENT '登录类型名称',
+  enabled BIT DEFAULT 1 NOT NULL
+  COMMENT '是否启用',
+  PRIMARY KEY (id)
 );
-create table ac_third_party_login(
-	user_id bigint(20) unsigned not null comment '用户ID',
-	login_type_id tinyint not null comment '登陆类型'
-	third_party_login_id bigint(20) unsigned not null comment '第三方登录Id',
-	primary key (user_id,login_type_id,third_party_login_id)
+CREATE TABLE ac_third_party_login (
+  user_id              BIGINT(20) UNSIGNED NOT NULL
+  COMMENT '用户ID',
+  login_type_id        TINYINT             NOT NULL
+  COMMENT '登陆类型',
+  third_party_login_id BIGINT(20) UNSIGNED NOT NULL
+  COMMENT '第三方登录Id',
+  PRIMARY KEY (user_id, login_type_id, third_party_login_id)
 );
-create table ac_session(
-	id bigint(20) unsigned not null ,
-	token varchar(200) not null '用户令牌',
-	user_id bigint(20) not null '用户id',
-	expire_seconds int default(604800) not null comment '',
-	create_on datetime not null default current_timestamp comment '创建时间'
+CREATE TABLE ac_session (
+  token          VARCHAR(200)             NOT NULL
+  COMMENT '用户令牌',
+  user_id        BIGINT(20)               NOT NULL
+  COMMENT '用户id',
+  expire_seconds INT DEFAULT '604800' NOT NULL
+  COMMENT '令牌过期时间',
+  create_on      DATETIME                 NOT NULL DEFAULT current_timestamp
+  COMMENT '创建时间',
+  PRIMARY KEY (token)
+);
+
+CREATE TABLE ac_app_module (
+  id          INT UNSIGNED        NOT NULL
+  COMMENT '模块id',
+  module_key  VARCHAR(64)         NOT NULL
+  COMMENT '模块key',
+  name        VARCHAR(64)         NOT NULL
+  COMMENT '模块名',
+  description VARCHAR(200)        NOT NULL
+  COMMENT '模块描述',
+  create_on   DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  create_by   BIGINT(20) UNSIGNED NOT NULL
+  COMMENT '创建者',
+  PRIMARY KEY (id)
 );
