fae58ffe06dee691d34430a3619a317e6874eb9a
921295877@qq.com <lipengju9219295877>
2018-08-02 08:11:58 +0800

    ..

---
+++ ActualCombat/ac-core/src/main/java/cn/asunboy/lang/ModifiedOnSupport.java
@@ -0,0 +1,12 @@
+package cn.asunboy.lang;
+
+import java.util.Date;
+
+/**
+ * @author LiPengJu
+ * @date 2018/7/31
+ */
+public interface ModifiedOnSupport {
+    Date getModifiedOn();
+    void setModifiedOn(Date modifiedOn);
+}
---
+++ ActualCombat/ac-core/src/main/java/cn/asunboy/lang/VersionSupport.java
@@ -0,0 +1,11 @@
+package cn.asunboy.lang;
+
+import java.util.Date;
+
+/**
+ * @author LiPengJu
+ * @date 2018/7/31
+ */
+public interface VersionSupport extends ModifiedOnSupport{
+    Date
+}
