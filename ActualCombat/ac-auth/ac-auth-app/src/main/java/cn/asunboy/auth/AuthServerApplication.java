7304d556bf77269d8a8e0f745f25a30299bb53a9
921295877@qq.com <lipengju9219295877>
2018-08-02 08:10:44 +0800

    ..

--- ActualCombat/ac-auth/ac-auth-app/src/main/java/cn/asunboy/auth/AuthServerApplication.java
+++ ActualCombat/ac-auth/ac-auth-app/src/main/java/cn/asunboy/auth/AuthServerApplication.java
@@ -2,6 +2,7 @@ package cn.asunboy.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户认证服务
--- ActualCombat/ac-auth/ac-auth-service/src/main/java/cn/asunboy/auth/service/impl/AcUserServiceImpl.java
+++ ActualCombat/ac-auth/ac-auth-service/src/main/java/cn/asunboy/auth/service/impl/AcUserServiceImpl.java
@@ -1,19 +1,42 @@
 package cn.asunboy.auth.service.impl;
 
 import cn.asunboy.auth.data.AcUserMapper;
+import cn.asunboy.auth.entity.AcUser;
 import cn.asunboy.auth.service.AcUserServcie;
-import cn.asunboy.entity.AcUser;
+
+import java.util.List;
 
 /**
+ * 用户服务实现
+ *
  * @author LiPengJu
  * @date 2018/7/25
  */
 public class AcUserServiceImpl implements AcUserServcie {
+    private final AcUserMapper userMapper;
+
+    public AcUserServiceImpl(AcUserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     @Override
-    public Long createUser() {
-        // 逻辑判断
-//        AcUserMapper
-        // 插入数据
+    public Long createUser(AcUser user) {
+        user.setCreatedOn(null);
+        return null;
+    }
+
+    @Override
+    public void updateUser(AcUser user) {
+
+    }
+
+    @Override
+    public AcUser getUserByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public List<AcUser> listUser() {
         return null;
     }
 }
